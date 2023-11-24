32888cb15e2eb49b663cf480a50edbae334ea218
Ignacio Henriquez <ignacio.henriquez@evalueserve.com>
2023-11-24 12:16:11 -0300

    uuid and pipe for validate uuid

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -4,7 +4,7 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
@@ -19,12 +19,12 @@ export class CarsController {
   }
 
   @Get('/:id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
+  getCarById(@Param('id', new ParseUUIDPipe({ version: '5' })) id: string) {
     // if (isNaN(+id)) {
     //   return 'ID should be a number';
     // }
     console.log({ id });
-    return this.carsService.findOneById(+id);
+    return this.carsService.findOneById(id);
   }
 
   @Post()
@@ -33,12 +33,12 @@ export class CarsController {
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+  updateCar(@Param('id') id: string, @Body() body: any) {
     return body;
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
+  deleteCar(@Param('id') id: number) {
     return { method: 'DELETE', id };
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { Car } from './interfaces/car.interface';
 
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 1, brand: 'Toyota', model: 'Corolla' },
-    { id: 2, brand: 'Honda', model: 'Civic' },
-    { id: 3, brand: 'Jeep', model: 'Cherokee' },
+  private cars: Car[] = [
+    { id: uuid(), brand: 'Toyota', model: 'Corolla' },
+    { id: uuid(), brand: 'Honda', model: 'Civic' },
+    { id: uuid(), brand: 'Jeep', model: 'Cherokee' },
   ];
 
   findAll() {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id ${id} not found`);
     return car;
